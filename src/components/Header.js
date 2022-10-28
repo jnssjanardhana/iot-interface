40ce8e8517c61fc5dcc58a7dd4ac20b42a01ae62
jnssjanardhana <janardhana.j@vishnu.edu.in>
2022-10-28 16:01:18 +0530

    Added Source Code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./Header.css";
+import logo from "./images/logo.png";
 
 function Header() {
   return (
     <div className = "header">
+      <img src = {logo} alt = " " />
       <h1>Smart Water Grid</h1>
       <div className = "options">
         <Link to = "/values">Pressure Values</Link>
